37c0af223013c82a45620f5e8a29eaa861d878ca
saliba-lab <56386981+saliba-lab@users.noreply.github.com>
2021-02-02 12:03:48 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,13 +8,9 @@ mkdir $dir/cutadapt_output $dir/alignment  $dir/non_unique_count $dir/unique_cou
 
 ### Assigning the sequence of primers
 GAT27dT=GTGAGTGATGGTTGAGGATGTGTGGAGNNNNN
-GAT27dTRC=NNNNNCTCCACACATCCTCAACCATCACTCAC
 GAT216N3G=GATGGTTGAGGATGTGTGGAGNNNNNN
-GAT216N3GRC=NNNNNNCTCCACACATCCTCAACCATC
 GAT27PCR=GTGAGTGATGGTTGAGGATGTGTGGAG
-GAT27PCRRC=CTCCACACATCCTCAACCATCACTCAC
 Nextra=CTGTCTCTTATA
-NextraRC=TATAAGAGACAG
 
 ### Running Cutadapt
 for i in {1..131}
@@ -26,10 +22,10 @@ read2="$(find /vol/data/gmak/runs/analyzed/novaseq/200220_A00278_0129_AHFKCGDRXX
 report="$(find /vol/data/gmak/runs/analyzed/novaseq/200220_A00278_0129_AHFKCGDRXX/HIRI_SIGA/ -name '*_R1_001.fastq.gz' -printf "%f\n" | sort | sed -n "$i"p | awk '{gsub("_R1_001.fastq.gz", "", $0); print}')"
 ##### Running the cutadapt command
 cutadapt -j 20 -u 20 -U 20 -u -3 -U -3 -m 20 -q 30 -a "A{100}" -A "A{100}" \
-    -g $GAT27dT -G $GAT27dT -g $GAT27dTRC -G $GAT27dTRC \
-    -g $GAT216N3G -G $GAT216N3G -g $GAT216N3GRC -G $GAT216N3GRC \
-    -g $GAT27PCR -G $GAT27PCR -g $GAT27PCRRC -G $GAT27PCRRC \
-    -a $Nextra -A $Nextra -a $NextraRC -A $NextraRC \
+    -g $GAT27dT -G $GAT27dT \
+    -g $GAT216N3G -G $GAT216N3G \
+    -g $GAT27PCR -G $GAT27PCR \
+    -a $Nextra -A $Nextra \
     -o $dir/cutadapt_output/$read1 -p $dir/cutadapt_output/$read2 /vol/data/gmak/runs/analyzed/novaseq/200220_A00278_0129_AHFKCGDRXX/HIRI_SIGA/$read1 /vol/data/gmak/runs/analyzed/novaseq/200220_A00278_0129_AHFKCGDRXX/HIRI_SIGA/$read2 \
     > $dir/cutadapt_output/$report.txt
 
